d8505f4fd84534ecd87701898ac1dd105ab40481
junelmujar <junelmujar@gmail.com>
2014-04-15 14:47:32 +0800

    Added transforms method, minor beautification, etc
    Added Column->transforms($transform) method to allow further modification of the cell values using Blade syntax
    
    Example: $dg->add('fullname', 'Full Name', true)->transforms('<div class="cell-title">{{ $fullname }}</div>{{ $address }}');
    
    Modified constructor to set the attributes and transform parameter values

--- src/Zofe/Rapyd/DataGrid/Column.php
+++ src/Zofe/Rapyd/DataGrid/Column.php
@@ -5,31 +5,34 @@ use Zofe\Rapyd\Widget;
 class Column extends Widget
 {
 
-    public $url = "";
+    public $url             = "";
+        
+    public $link            = "";
+    public $linkRoute       = "";
     
-    
-    public $link = "";
-    public $linkRoute = "";
-    
-    public $label = "";
-    public $attributes = array();
-    public $tr_attributes = array();
-    public $tr_attr = array();
-    public $orderby = null;
+    public $label           = "";
+    public $attributes      = array();
+    public $transform       = null;
+    public $tr_attributes   = array();
+    public $tr_attr         = array();
+    public $orderby         = null;
+    protected $pattern      = "";
+    protected $pattern_type = null;
+    protected $row_as       = null;
+    protected $field        = null;
+    protected $field_name   = null;
+    protected $field_list   = array();
+
     public $orderby_asc_url;
     public $orderby_desc_url;
-    protected $pattern = "";
-    protected $pattern_type = null;
-    protected $row_as = null;
-    protected $field = null;
-    protected $field_name = null;
-    protected $field_list = array();
 
     public function __construct($name, $label = null, $orderby = false)
     {
         $this->name = $name;
         $this->label($label);
         $this->orderby($orderby);
+        $this->attributes($attributes);
+        $this->transforms($transform);        
     }
 
     protected function label($label)
@@ -56,4 +59,9 @@ class Column extends Widget
         return $this;
     }
     
+    public function transforms($transform)
+    {
+        $this->transform = $transform;
+        return $this;
+    }    
 }
