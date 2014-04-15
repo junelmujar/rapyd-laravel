bf319a6cc8da03206f3bb6ff2a7999049ec148e6
junelmujar <junelmujar@gmail.com>
2014-04-15 15:08:24 +0800

    Updated datagrid view
    - Modified column headers to indicate only the active sorting direction
    - Converted the column text to link as the sorting toggle

--- src/views/datagrid.blade.php
+++ src/views/datagrid.blade.php
@@ -1,45 +1,48 @@
-
 @include('rapyd::toolbar', array('label'=>$label, 'buttons'=>$buttons['TR']))
 
-<table class="table table-striped">
-    <thead>
-    <tr>
-     @foreach ($dg->columns as $column)
-            <th>
-            @if ($column->orderby)
-                @if ($column->name != Input::get('ord'))
-                 <a href="{{ $dg->orderbyLink($column->name,'asc') }}">
-                     <span class="glyphicon glyphicon-arrow-up"></span>
-                 </a>
-                @else
-                    <span class="glyphicon glyphicon-arrow-up"></span>
-                @endif
-                @if ('-'.$column->name != Input::get('ord'))
-                 <a href="{{ $dg->orderbyLink($column->name,'desc') }}">
-                     <span class="glyphicon glyphicon-arrow-down"></span>
-                 </a>
-                @else
-                    <span class="glyphicon glyphicon-arrow-down"></span>
-                @endif
-                {{ $column->label }}
-            @else
-                {{ $column->label }}
-            @endif
-            </th> 
-     @endforeach
-    </tr>
-    </thead>
-    <tbody>
-    @foreach ($dg->rows as $row)
+@if ($dg->rows) 
+    <table class="table table-striped">
+        <thead>
         <tr>
-            @foreach ($row as $cell)
-            <td>{{ $cell }}</td>
-            @endforeach
+         @foreach ($dg->columns as $column)
+            <th>
+                @if ($column->orderby)
+                    @if (($column->name == Input::get('ord')) || ('-'.$column->name == Input::get('ord')))
+                        @if ('-'.$column->name == Input::get('ord'))
+                            <a href="{{ $dg->orderbyLink($column->name,'asc') }}">{{ $column->label }}</a> &nbsp;
+                           <span class="glyphicon glyphicon-arrow-up"></span>
+                        @else
+                            <a href="{{ $dg->orderbyLink($column->name,'desc') }}">{{ $column->label }}</a> &nbsp;
+                            <span class="glyphicon glyphicon-arrow-down"></span>
+                        @endif
+                    @else
+                        <a href="{{ $dg->orderbyLink($column->name,'asc') }}">{{ $column->label }}</a> &nbsp;
+                    @endif
+    
+                @else
+                    {{ $column->label }}
+                @endif
+            </th>             
+         @endforeach
         </tr>
-    @endforeach
-    </tbody> 
-</table>
+        </thead>
+        <tbody>
+        @foreach ($dg->rows as $row)
+            <tr>
+                @foreach ($row as $cell)
+                <td>{{ $cell }}</td>
+                @endforeach
+            </tr>
+        @endforeach
+        </tbody> 
+    </table>
+@else
 
+    <h3>
+        There are no records in the database.
+    </h3>
+
+@endif
 
 @if ($dg->havePagination())
     <div class="pagination">
